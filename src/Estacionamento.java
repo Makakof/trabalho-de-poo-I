ac2ce59f214ab80b14baad757be7146aa918362d
ebata-rafael <ebata.rafael@ufms.br>
2024-06-02 16:10:28 -0300

    "trabalho finalizado"

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -1,12 +1,5 @@
 
-import automovel.Cor;
-import automovel.Modelo;
-import automovel.Veiculo;
-import cliente.estacionabem.Cliente;
 import dados.Repositorio;
-import enums.DiaDaSemana;
-import enums.HoristaMensalista;
-import enums.TipoVeiculo;
 import excecoes.EstacionamentoException;
 
 import kitmenu.MenuGerenciaCliente;
@@ -14,13 +7,8 @@ import kitmenu.MenuGerenciaEstacionamento;
 import kitmenu.MenuGerenciaVagas;
 import kitmenu.MenuFaturamento;
 import interfaces.Terminal;
-import modelagem.Vaga;
-import tarifacao.TarifaEstacionamento;
-import tarifacao.TarifaHorista;
-import tarifacao.TarifaMensalista;
 import utilitarios.UpdateUtils;
 
-import java.util.ArrayList;
 
 
 public class Estacionamento
@@ -37,22 +25,6 @@ public class Estacionamento
 
         UpdateUtils.atualizarStatusDasVagas(Repositorio.getInstance().getTickets());
 
-        //entradas para testar
-        Cliente cliente = new Cliente("rafael", "000");
-        cliente.addVeiculo(new Veiculo("A0", new Cor("preto"), new Modelo("corsa"), TipoVeiculo.CARRO));
-        Vaga vaga = new Vaga(10, "rio de janeiro", TipoVeiculo.CARRO);
-
-        Repositorio.getInstance().getClientes().add(cliente);
-        Repositorio.getInstance().getVagas().add(vaga);
-
-        ArrayList<DiaDaSemana> dias = new ArrayList<>();
-        dias.add(DiaDaSemana.DOMINGO);
-
-        TarifaEstacionamento tarifa = new TarifaHorista(10, 12, dias, HoristaMensalista.HORISTA);
-        Repositorio.getInstance().getTarifas().add(tarifa);
-
-        tarifa = new TarifaMensalista(120, dias, HoristaMensalista.MENSALISTA);
-        Repositorio.getInstance().getTarifas().add(tarifa);
 
         try 
         {
