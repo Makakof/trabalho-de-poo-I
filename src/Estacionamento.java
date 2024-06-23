e187a42467d064999eb15d1a15a9b2d8d1fadd60
Makakof <luiz.h.s.silva@ufms.br>
2024-06-23 14:44:55 -0300

    decisao de singleton, menu principal, interface grafica

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -1,14 +1,10 @@
 
 import dados.Repositorio;
-import enums.OpcaoMenuPrincipal;
 import excecoes.EstacionamentoException;
-
-import kitmenu.MenuGerenciaCliente;
-import kitmenu.MenuGerenciaEstacionamento;
-import kitmenu.MenuGerenciaVagas;
-import operacoes.FuncionalidadesFaturamento;
-import interfaces.Terminal;
+import kitmenu.MenuPrincipal;
+import operacoes.FuncionalidadeInterface;
 import utilitarios.UpdateUtils;
+
 
 
 
@@ -16,44 +12,12 @@ public class Estacionamento
 {
     public static void main(String[] args)
     {
-
-        Terminal terminal = Terminal.getInstance();
-        FuncionalidadesFaturamento menuFaturamento = new FuncionalidadesFaturamento();
-        byte opcaoPrincipal;
-
         UpdateUtils.atualizarStatusDasVagas(Repositorio.getInstance().getTickets());
-
+        FuncionalidadeInterface.EscolheInterface();
 
         try 
         {
-            do 
-            {
-                terminal.exibirMenuPrincipal();
-                opcaoPrincipal = terminal.selecionarByte("Digite a opção desejada: ");
-
-                switch (opcaoPrincipal)
-                {
-                    case 1:
-                        MenuGerenciaCliente.gerenciarCliente();
-                        break;
-                    case 2:
-                        MenuGerenciaVagas.GerenciarVagas();
-                        break;
-                    case 3:
-                        MenuGerenciaEstacionamento.gerenciarEstacionamento();
-                        break;
-                    case 4:
-                        terminal.exibir("Sem funcionalidades no momento");
-                        break;
-                    case 5:
-                        menuFaturamento.realizarFaturamento();
-                        break;
-                    case 6: //voltar
-                        break;
-                    default:
-                        throw new EstacionamentoException("Opção inválida de menu");
-                }
-            }while(opcaoPrincipal != OpcaoMenuPrincipal.SAIR.ordinal()+1);
+            MenuPrincipal.menuPrincial();
         }
         catch (EstacionamentoException msg){
             System.out.println(msg);
--- src/operacoes/FuncionalidadesEstacionamento.java
+++ src/operacoes/FuncionalidadesEstacionamento.java
@@ -23,7 +23,7 @@ public class FuncionalidadesEstacionamento {
     private final InterfaceUsuario interfaceUsuario;
 
     public FuncionalidadesEstacionamento() {
-        this.interfaceUsuario = Terminal.getInstance();
+        interfaceUsuario = Repositorio.getInstance().getUI();
     }
 
     public TicketEstacionamento estacionar(ArrayList<Cliente> clientes, ArrayList<TarifaEstacionamento> tarifas, ArrayList<Vaga> vagas) {
